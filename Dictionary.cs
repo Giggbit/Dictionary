91258ae27079ecc996fa63e2691b5b3a6afb4bf0
Giggbit <danya19981017@gmail.com>
2023-03-13 12:55:43 +0100

    Добавьте файлы проекта.

---
+++ Dictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_task_Dictionary
+{
+    interface CreateDictionary {
+        void Create();
+    }
+    interface EditDictionary {
+        void Add(Dictionary<string, string> dic, FileStream fs);
+        
+    }
+    interface PrintDictionary {
+        void Print();
+    }
+
+    class Dictionary : CreateDictionary, EditDictionary, PrintDictionary
+    {
+        public Dictionary<string, string> dictionary;
+        public string type;
+
+        public Dictionary() { }    
+        public Dictionary(Dictionary<string, string> word) {
+            dictionary = word;
+        }
+
+        public void Create() {
+            dictionary = new Dictionary<string, string>();
+            Console.Write("Enter type of dictionary: ");
+            type = Console.ReadLine();
+        }
+
+        public void Add(Dictionary<string, string> dic, FileStream fs) {
+            Console.Write("Enter word: ");
+            string word = Console.ReadLine();
+            Console.Write("Enter translate word: ");
+            string t_word = Console.ReadLine();
+            dic.Add(word, t_word);
+            /*StreamWriter write_file = new StreamWriter(fs);
+            write_file.WriteLine(dic);
+            foreach(var item in dic) {
+                write_file.Write($"{item.Key} - {item.Value}");
+            }*/
+        }
+
+        public void Print() {
+            Console.WriteLine($"Type: {type}");
+            foreach(var a in dictionary) {
+                Console.WriteLine($"{a.Key} - {a.Value}");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_task_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary dict = new Dictionary();
+            dict.Create();
+            FileStream file = new FileStream(dict.type + ".txt", FileMode.OpenOrCreate);
+
+            dict.Add(dict.dictionary, file);
+            dict.Print();
+            file.Close();
+
+
+        }
+    }
+}
