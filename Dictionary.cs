b77010473633afde83efc662b7b6674151cadf4c
Giggbit <danya19981017@gmail.com>
2023-03-14 03:41:59 +0100

    Completed

--- Dictionary.cs
+++ Dictionary.cs
@@ -11,8 +11,10 @@ namespace Exam_task_Dictionary
         void Create();
     }
     interface EditDictionary {
-        void Add(Dictionary<string, string> dic, FileStream fs);
-        
+        void Add(Dictionary<string, string> dic, FileStream file, StreamWriter sw);
+        void Edit(Dictionary<string, string> dic, string file);
+        void Delete(Dictionary<string, string> dic, string file);
+        void Find(Dictionary<string, string> dic);
     }
     interface PrintDictionary {
         void Print();
@@ -32,26 +34,82 @@ namespace Exam_task_Dictionary
             dictionary = new Dictionary<string, string>();
             Console.Write("Enter type of dictionary: ");
             type = Console.ReadLine();
+            FileStream file = new FileStream(type + ".txt", FileMode.OpenOrCreate);
+            //StreamWriter sw = new StreamWriter(file);
+            file.Close();
         }
 
-        public void Add(Dictionary<string, string> dic, FileStream fs) {
+        public void Add(Dictionary<string, string> dic, FileStream file, StreamWriter sw) {
             Console.Write("Enter word: ");
             string word = Console.ReadLine();
             Console.Write("Enter translate word: ");
             string t_word = Console.ReadLine();
             dic.Add(word, t_word);
-            /*StreamWriter write_file = new StreamWriter(fs);
-            write_file.WriteLine(dic);
-            foreach(var item in dic) {
-                write_file.Write($"{item.Key} - {item.Value}");
-            }*/
+            sw.WriteLine($"{word} - {t_word}");
+        }
+
+        public void Edit(Dictionary<string, string> dic, string file) {
+            Console.Write("Enter word to change it: ");
+            string find = Console.ReadLine();
+            if (dic.ContainsKey(find)) {
+                var lines = File.ReadAllLines(file).ToList();
+                int index = lines.IndexOf(find + " - " + dic[find]);
+
+                Console.Write("Enter new word: ");
+                string word = Console.ReadLine();
+                Console.Write("Enter new translate: ");
+                string translate = Console.ReadLine();
+                dic.Remove(find);
+                lines.RemoveAt(index);
+                File.WriteAllLines($"{file}", lines);
+                dic.Add(word, translate);
+                string text = null;
+                foreach(var i in dic) {
+                    text += $"{i.Key} - {i.Value}\n";
+                }
+                File.WriteAllText(file,text);
+            }
+            else { Console.WriteLine("Didn't find this word!"); }
+        }
+
+        public void Delete(Dictionary<string, string> dic, string file) {
+            Console.Write("Enter word to delete: ");
+            string word = Console.ReadLine();
+            var lines = File.ReadAllLines(file).ToList();
+            int index = lines.IndexOf(word + " - " + dic[word]);
+
+            if (dic.ContainsKey(word)) {
+                dic.Remove(word);
+                lines.RemoveAt(index);
+                File.WriteAllLines(file, lines);
+            }
+        }
+
+        public void Find(Dictionary<string, string> dic) { 
+            if(dic != null) {
+                Console.Write("Enter word to find it: ");
+                string word = Console.ReadLine();
+                if (dic.ContainsKey(word)) {
+                    Console.WriteLine($"{word} - {dic[word]}");
+                }
+                else {
+                    foreach (var key in dic.Keys) {
+                        if (dic[key] == word) {
+                            Console.WriteLine($"{key} - {dic[key]}");
+                        }
+                    } 
+                }
+            }
         }
 
         public void Print() {
-            Console.WriteLine($"Type: {type}");
-            foreach(var a in dictionary) {
-                Console.WriteLine($"{a.Key} - {a.Value}");
-            }
+            Console.WriteLine($"  Type: {type}");
+            /*foreach(var a in dictionary) {
+                Console.WriteLine($"\t{a.Key} - {a.Value}");
+            }*/
+            StreamReader sr = new StreamReader(type + ".txt", Encoding.UTF8);
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -11,13 +11,65 @@ namespace Exam_task_Dictionary
     {
         static void Main(string[] args)
         {
-            Dictionary dict = new Dictionary();
+            /*Dictionary dict = new Dictionary();
             dict.Create();
-            FileStream file = new FileStream(dict.type + ".txt", FileMode.OpenOrCreate);
+            FileStream fs = new FileStream(dict.type + ".txt", FileMode.Open);
+            StreamWriter sw = new StreamWriter(fs);
+            dict.Add(dict.dictionary, fs, sw);
+            dict.Add(dict.dictionary, fs, sw);
+            sw.Close();
 
-            dict.Add(dict.dictionary, file);
-            dict.Print();
-            file.Close();
+            //dict.Delete(dict.dictionary, dict.type + ".txt");
+            //dict.Edit(dict.dictionary, dict.type + ".txt");
+            dict.Print();*/
+
+
+            Dictionary dict = new Dictionary();
+
+            Console.WriteLine("Welcome to dictionary!");
+            bool exit = false;
+            while (exit != true) {
+                Console.Write("Choose process:   1.Create  2.Add  3.Edit  4.Delete  5.Print  6.Exit\nAnswer: ");
+                try {
+                    string c = Console.ReadLine();
+                    int choose = Convert.ToInt32(c);
+                    switch (choose) {
+                        case 1:
+                            dict.Create();
+                            Console.WriteLine();
+                            break;
+
+                        case 2:
+                            FileStream fs = new FileStream(dict.type + ".txt", FileMode.Append);
+                            StreamWriter sw = new StreamWriter(fs);
+                            dict.Add(dict.dictionary, fs, sw);
+                            sw.Close();
+                            
+                            break;
+
+                        case 3:
+                            dict.Edit(dict.dictionary, dict.type + ".txt");
+                            Console.WriteLine();
+                            break;
+
+                        case 4:
+                            dict.Delete(dict.dictionary, dict.type + ".txt");
+                            Console.WriteLine();
+                            break;
+
+                        case 5:
+                            dict.Print();
+                            Console.WriteLine();
+                            break;
+
+                        case 6:
+                            exit = true;
+                            break;
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine($"\nОшибка!\n{ex.Message}\n"); }
+            }
+
 
 
         }
